a22a400432a6b12f871c7821c9a878828ee4a746
v.shustov <57774085+shuimi@users.noreply.github.com>
2021-10-23 15:07:48 +0300

    -camera and mouse listener added

---
+++ src/components/webgl/event-handlers/mouse-listener.js
@@ -0,0 +1,53 @@
+import GLW from '../gl-wrapper';
+
+class MouseListener {
+    constructor(){
+        this.onWheelListeners = [];
+        this.onDragListeners = []
+    }
+
+    init = () => {
+        let x = 0;
+        let y = 0;
+        let dragging = false;
+
+        GLW.gl.canvas.onwheel = (e) => {
+            this.onWheelListeners.forEach(listener => {
+                listener.onWheel(e);
+            })
+        }
+
+        GLW.gl.canvas.onmousedown = (e) => {
+            x = e.clientX;
+            y = e.clientY;
+            dragging = true;
+        }
+
+        GLW.gl.canvas.onmouseup = () => {
+            dragging = false;
+        }
+
+        GLW.gl.canvas.onmousemove = (e) => {
+            if (dragging) {
+                const dx = x - e.clientX;
+                const dy = y - e.clientY;
+                x = e.clientX;
+                y = e.clientY;
+                this.onDragListeners.forEach(listener => {
+                    listener.onDrag(dx, dy);
+                });
+            }
+        }
+    }
+
+    subscribeToDrag = (listener) => {
+        this.onDragListeners.push(listener);
+    }
+
+    subscribeToWheel = (listener) => {
+        this.onWheelListeners.push(listener)
+    }
+}
+
+const MouseEvent = new MouseListener();
+export default MouseEvent;
